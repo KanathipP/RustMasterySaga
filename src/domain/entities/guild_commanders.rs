947f34a4e12a175f74499e46caf0566d2508760c
KanathipP <kanathipz.p@gmail.com>
2025-04-08 06:37:04 +0700

    change adventurers table name in guild commanders domain to guild_commanders

--- src/domain/entities/guild_commanders.rs
+++ src/domain/entities/guild_commanders.rs
@@ -1,11 +1,11 @@
 use chrono::NaiveDateTime;
 use diesel::{prelude::{Identifiable, Insertable, Queryable}, Selectable};
 
-use crate::infrastructure::postgres::schema::adventurers;
+use crate::infrastructure::postgres::schema::guild_commanders;
 
 
 #[derive(Debug,Clone,Identifiable,Selectable,Queryable)]
-#[diesel(table_name = adventurers)]
+#[diesel(table_name = guild_commanders)]
 pub struct GuildCommanderEntity {
     pub id:i32,
     pub username: String,
@@ -15,7 +15,7 @@ pub struct GuildCommanderEntity {
 }
 
 #[derive(Debug,Clone,Insertable,Queryable)]
-#[diesel(table_name = adventurers)]
+#[diesel(table_name = guild_commanders)]
 pub struct RegisterGuildCommanderEntity {
     pub username: String,
     pub password: String,
