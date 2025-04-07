99c21d6da3c014e0b73573b16f3a7ce91215120f
KanathipP <kanathipz.p@gmail.com>
2025-04-07 15:55:52 +0700

    Register (not finished)

--- src/application/usecases/adventurers.rs
+++ src/application/usecases/adventurers.rs
@@ -2,11 +2,11 @@ use std::sync::Arc;
 
 use anyhow::Result;
 
-use crate::domain::{
+use crate::{domain::{
     entities::adventurers::RegisterAdventurerEntity,
     repositories::adventurers::AdventurersRepository,
     value_objects::adventurer_model::RegisterAdventurerModel,
-};
+}, infrastructure::argon2_hashing};
 
 pub struct AdventurersUseCase<T>
 where
@@ -29,10 +29,15 @@ where
         &self,
         mut register_adventurer_model: RegisterAdventurerModel,
     ) -> Result<i32> {
-        unimplemented!()
+        let hashed_password = argon2_hashing::hash(register_adventurer_model.password.clone())?;
+
+        register_adventurer_model.password = hashed_password;
+
+        let register_entity = register_adventurer_model.to_entity();
+
+        let adventurer_id= self.adventurers_repository.register(register_entity).await?;
+
+        Ok(adventurer_id)
     }
 }
 
-// Handler -> Axum
-// UseCase -> Application
-// Repository
--- src/infrastructure/postgres/repositories/adventurers.rs
+++ src/infrastructure/postgres/repositories/adventurers.rs
@@ -1,13 +1,14 @@
 use std::sync::Arc;
 
-use anyhow::Result;
+use anyhow::{Ok, Result};
 use axum::async_trait;
+use diesel::{dsl::insert_into, prelude::*};
 
 use crate::{
     domain::{
-        entities::adventurers::{AdventurerEntity, RegisterAdventurerEntity}, repositories::adventurers::AdventurersRepository,
+        entities::adventurers::{AdventurerEntity, RegisterAdventurerEntity}, repositories::adventurers:: AdventurersRepository,
     },
-    infrastructure::postgres::postgres_connection::PgPoolSquad,
+    infrastructure::postgres::{postgres_connection::PgPoolSquad, schema::adventurers},
 };
 
 pub struct AdventurerPostgres {
@@ -23,9 +24,23 @@ impl AdventurerPostgres {
 #[async_trait]
 impl AdventurersRepository for AdventurerPostgres {
     async fn register(&self, register_adventurer_entity: RegisterAdventurerEntity) -> Result<i32> {
-        unimplemented!()
+        let mut conn = Arc::clone(&self.db_pool).get()?;
+
+        let result = insert_into(adventurers::table)
+        .values(register_adventurer_entity)
+        .returning(adventurers::id)
+        .get_result::<i32>(&mut conn)?;
+
+        Ok(result)
     }
     async fn find_by_username(&self, username: String) -> Result<AdventurerEntity> {
-        unimplemented!()
+        let mut conn = Arc::clone(&self.db_pool).get()?;
+
+        let result = adventurers::table
+            .filter(adventurers::username.eq(username))
+            .select(AdventurerEntity::as_select())
+            .first::<AdventurerEntity>(&mut conn)?;
+
+        Ok(result)
     }
 }
